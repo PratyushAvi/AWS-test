7cbdc2441d7efd20e703def3cd512c749c45d7eb
Pratyush Avi <avipratyush2000@gmail.com>
2026-02-13 15:52:41 -0500

    added skip for non-essential tests

--- batched_float_test.py
+++ batched_float_test.py
@@ -10,14 +10,15 @@ def main():
     parser.add_argument('--cap', type=int, default=50, help='Dataset capacity (default: 50)')
     parser.add_argument('--dim', type=int, default=100, help='Vector dimensions (default: 100)')
     parser.add_argument('--tests', type=int, default=10, help='Number of tests to run (default: 10)')
-    
+    parser.add_argument('--skip', type=bool, default=False, help='Skip non-essential tests') 
     args = parser.parse_args()
     
     BATCH = args.batch
     CAP = args.cap
     DIM = args.dim
     TESTS = args.tests
-   
+    SKIPS = args.skip
+
     print(f"------------\nBATCHED float32 TEST\n------------")
     print(f"BATCH SIZE: {BATCH}, TESTS: {TESTS}")
     ram, used_ram, num_vectors, dataset = fill_ram_with_float32_vectors(CAP, DIM, with_stats=True)
@@ -33,7 +34,7 @@ def main():
         norm = np.linalg.norm(vecs, axis=1) ** 2
         times.append(time() - start)
     results.append(sum(times)/len(times))
-    
+        
     # Test 2: L2 Norm - einsum
     times = []
     for i in tqdm(range(min(TESTS, dataset.shape[0]))):
@@ -65,39 +66,46 @@ def main():
         times.append(time() - start)
     results.append(sum(times)/len(times))
     
-    # Test 5: Matrix-Matrix Product - einsum
-    times = []
-    for i in tqdm(range(min(TESTS, dataset.shape[0]))):
-        vecs = dataset[i:i + BATCH]
-        start = time()
-        matvecs_einsum = np.einsum('ij,kj->ik', dataset, vecs)
-        times.append(time() - start)
-    results.append(sum(times)/len(times))
+    if SKIPS:
+       results.append(None)
+    else:
+        # Test 5: Matrix-Matrix Product - einsum
+        times = []
+        for i in tqdm(range(min(TESTS, dataset.shape[0]))):
+            vecs = dataset[i:i + BATCH]
+            start = time()
+            matvecs_einsum = np.einsum('ij,kj->ik', dataset, vecs)
+            times.append(time() - start)
+        results.append(sum(times)/len(times))
     
     print(f"Avg. Matrix-Matrix Product Computation Time (matmul): {results[3]}")
     print(f"Avg. Matrix-Matrix Product Computation Time (einsum): {results[4]}")
     
-    # Test 6: Distance - linalg.norm
-    times = []
-    for i in tqdm(range(min(TESTS, dataset.shape[0]))):
-        vecs = dataset[i: i + BATCH]
-        start = time()
-        norms = (np.linalg.norm(vecs, axis=1)**2).astype(np.float32)
-        dot_prod = dataset @ vecs.T
-        distances = norms - 2 * dot_prod
-        times.append(time() - start)
-    results.append(sum(times)/len(times))
+    if SKIPS:
+        results.append(None)
+        results.append(None)
+    else:
+        # Test 6: Distance - linalg.norm
+        times = []
+        for i in tqdm(range(min(TESTS, dataset.shape[0]))):
+            vecs = dataset[i: i + BATCH]
+            start = time()
+            norms = (np.linalg.norm(vecs, axis=1)**2).astype(np.float32)
+            dot_prod = dataset @ vecs.T
+            distances = norms - 2 * dot_prod
+            times.append(time() - start)
+        results.append(sum(times)/len(times))
     
-    # Test 7: Distance - einsum
-    times = []
-    for i in tqdm(range(min(TESTS, dataset.shape[0]))):
-        vecs = dataset[i: i + BATCH]
-        start = time()
-        norms = np.einsum('ij,ij->i', vecs, vecs, dtype=np.float32)
-        dot_prod = dataset @ vecs.T
-        distances_einsum = norms - 2 * dot_prod
-        times.append(time() - start)
-    results.append(sum(times)/len(times))
+        # Test 7: Distance - einsum
+        times = []
+        for i in tqdm(range(min(TESTS, dataset.shape[0]))):
+            vecs = dataset[i: i + BATCH]
+            start = time()
+            norms = np.einsum('ij,ij->i', vecs, vecs, dtype=np.float32)
+            dot_prod = dataset @ vecs.T
+            distances_einsum = norms - 2 * dot_prod
+            times.append(time() - start)
+        results.append(sum(times)/len(times))
     
     # Test 8: Distance - sum of squares
     times = []
